1dbfabaa1b407837b96869c6feaecd51ca97ec07
Jacob Bloom <mr.jacob.bloom@gmail.com>
2016-06-11 14:07:07 -0600

    got "getBlocksWithText" working

--- README.md
+++ README.md
@@ -4,7 +4,29 @@ This set of tools should make it easier to write themes/skins for [Scratch-Block
 ```
 var t = new Sb3Theme();
 
+// "addInit" funcions only run when the editor is first opened
 t.addInit(function() {
-  t.addFilter('<filter id="myFilter"> ... </filter>');
+
+  // add a blur filter to the SVG
+  t.addFilter(`<filter id="myFilter">
+    <feGaussianBlur in="SourceGraphic" stdDeviation="3" y="-"/>
+  </filter>`);
+
+  // add the filter to all elements with the class "myRepeats"
+  t.css.innerHTML += `.myRepeats {
+    filter: url(#myFilter)
+  }`;
+});
+
+// "addOnChange" functions run whenever the number of blocks changes
+t.addOnChange(function() {
+  
+  // find all the blocks with the word "repeat"
+  var repeats = t.getBlocksWithText('repeat');
+
+  // give them all the class name "myRepeats"
+  repeats.forEach(function(elem) {
+    elem.classList.add("myRepeats");
+  });
 });
 ```
--- sb3-theme.js
+++ sb3-theme.js
@@ -1,9 +1,9 @@
 function Sb3Theme() {
   // hang out until the SVG exists, then run the init function
   var self = this;
-  var observer = new MutationObserver(function(mutations) {
+  var initObserver = new MutationObserver(function(mutations) {
     if(document.querySelector('svg.blocklySvg')) {
-      observer.disconnect();
+      initObserver.disconnect();
       self._initSVG();
     }
   });
@@ -11,28 +11,63 @@ function Sb3Theme() {
   if(document.querySelector('svg.blocklySvg')) {
     this._initSVG();
   } else {
-    observer.observe(document.getElementsByTagName('html')[0], {childList: true, subtree: true}); // <body> doesn't always exist at runtime
+    initObserver.observe(document.getElementsByTagName('html')[0], {childList: true, subtree: true}); // <body> doesn't always exist at runtime
   }
 
   this._initSVG = function() {
     this.svg = document.querySelector('svg.blocklySvg'); // Blockly.WorkspaceSVG.cachedParentSvg_;
-    //this.horizontal = Blockly.WorkspaceSVG.horizontalLayout;
-    this.css = Blockly.Css.styleSheet;
+
+    this.css = document.createElement("style");
+    document.body.appendChild(this.css);
+
     this.NS = Blockly.SVG_NS;
     this.defs = this.svg.getElementsByTagName('defs')[0];
     for(i in onLoads) {
       onLoads[i]();
     }
+
+    //set up an observer for future changes to the document
+    var draggableCount = -1;
+    var observer = new MutationObserver(function(mutations) {
+      self.draggables = self.svg.querySelectorAll(".blocklyDraggable")
+      if(draggableCount != self.draggables.length) {
+        draggableCount = self.draggables.length;
+        for(i in onChanges) {
+          onChanges[i]();
+        }
+      }
+    });
+
+    observer.observe(this.svg, {childList: true, subtree: true});
   }
 
   var onLoads = [];
   this.addInit = function(func) {
     onLoads.push(func);
   }
+  var onChanges = [];
+  this.addOnChange = function(func) {
+    onChanges.push(func);
+  }
 
   this.addFilter = function(filter) {
     var doc = new DOMParser().parseFromString(`<svg xmlns="` + this.NS + `">` + filter + `</svg>`, 'image/svg+xml');
     this.defs.appendChild( this.defs.ownerDocument.importNode(doc.documentElement.firstElementChild, true) );
+  }
+
+  this.getBlocksWithText = function(query) {
+    var result = [];
+    for(let i = 0; i < this.draggables.length; i++) {
+      let text = "";
+      for(let j = 0; j < this.draggables[i].getElementsByTagName('text').length; j++) {
+        text += " " + this.draggables[i].getElementsByTagName('text')[j].textContent;
+      }
+      text = text.replace(/(&nbsp;|  +)/g, " ")
+      if(text.match(query)) {
+        result.push( this.draggables[i].getElementsByClassName('blocklyPath')[0] );
+      }
+    }
+    return result;
   }
 
 }
